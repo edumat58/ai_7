81e3f77b853187177b0d82b6978d8c2715a429f1
edumat58 <asbri.sebastian@gmail.com>
2025-05-24 00:31:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-def model_summary():
-    import numpy as np
-    import pandas as pd
-    from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+from sklearn
 
-    # Sample data
-    X = np.array([[1], [2], [3], [4], [5]])
-    y = np.array([2, 4, 6, 8, 10])
+ratings = pd.read_csv("ratings.csv")
+ratings.head()
 
-    model = LinearRegression()
-    model.fit(X, y)
-
-    df = pd.DataFrame({
-        "X": X.flatten(),
-        "y": y,
-        "prediction": model.predict(X)
-    })
-
-    return df
-
-if __name__ == "__main__":
-    print(model_summary())
